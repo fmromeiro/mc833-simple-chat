4d4d75a474327304459891b7d07c66431e666bbe
Guilherme Brandt <GuiBrandt@users.noreply.github.com>
2022-12-08 16:14:52 -0300

    v0

---
+++ src/client.c
@@ -0,0 +1,191 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/param.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define max(a, b) (((a)>(b))?(a):(b))
+
+#define SERVER_PORT 8081
+static const char* server_address = "127.0.0.1";
+
+int main(int argc, char const *argv[]) {
+    int udp_fd;
+    if ((udp_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in udp_addr;
+    memset(&udp_addr, 0, sizeof(udp_addr));
+    udp_addr.sin_family = AF_INET;
+    udp_addr.sin_port = 0;
+
+    if ((bind(udp_fd, (struct sockaddr*)&udp_addr, sizeof(udp_addr))) < 0) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    int sock_fd;
+    struct sockaddr_in serv_addr;
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(SERVER_PORT);
+
+    if (inet_pton(AF_INET, server_address, &serv_addr.sin_addr) <= 0) {
+        perror("inet_pton failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(sock_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("connect failed");
+        exit(EXIT_FAILURE);
+    }
+
+    socklen_t addrlen = sizeof(struct sockaddr_in);
+    if (getsockname(udp_fd, (struct sockaddr*)&udp_addr, &addrlen) < 0) {
+        perror("getsockname failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int n = send(sock_fd, &udp_addr.sin_port, sizeof(in_port_t), 0);
+    if (n < 0) {
+        perror("send failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int user_fd = fileno(stdin);
+
+    struct sockaddr_in peer_addr;
+
+    while (true) {
+        printf("a\n");
+
+        fd_set fdset;
+        FD_ZERO(&fdset);
+        FD_SET(user_fd, &fdset);
+        FD_SET(udp_fd, &fdset);
+        FD_SET(sock_fd, &fdset);
+        
+        int nfds = max(max(user_fd, udp_fd), sock_fd) + 1;
+
+        int n = select(nfds, &fdset, NULL, NULL, NULL);
+        if (n == -1) {
+            perror("select failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("b\n");
+
+        if (FD_ISSET(udp_fd, &fdset)) {
+            char ping;
+
+            int n = recvfrom(udp_fd, &ping, 1, 0, (struct sockaddr*)&peer_addr, &addrlen);
+            if (n < 0) {
+                perror("recv failed");
+                exit(EXIT_FAILURE);
+            }
+
+            break;
+        }
+        
+        if (FD_ISSET(user_fd, &fdset)) {
+            int target;
+            scanf("%d", &target);
+
+            int bytes_sent = send(sock_fd, &target, sizeof(int), 0);
+            if (bytes_sent < 0) {
+                perror("send failed");
+                exit(EXIT_FAILURE);
+            }
+
+            recv(sock_fd, &peer_addr, sizeof(peer_addr), 0);
+            break;
+        }
+
+        if (FD_ISSET(sock_fd, &fdset)) {
+            char client_list_buf[1024] = {0};
+            int bytes_received = recv(sock_fd, client_list_buf, 1024, 0);
+            if (bytes_received < 0) {
+                perror("recv failed");
+                exit(EXIT_FAILURE);
+            }
+            printf("Clientes conectados:\n%s\n", client_list_buf);
+
+            printf("Escolha alguém para se conectar: ");
+            fflush(stdout);
+        }
+    }
+
+    char* target_ip = inet_ntoa(peer_addr.sin_addr);
+    if (target_ip == NULL) {
+        printf("Endereço IP inválido\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int target_port = ntohs(peer_addr.sin_port);
+    printf("Conectado a %s:%d", target_ip, target_port);
+
+    char message[1024] = {0};
+
+    printf("> ");
+    fflush(stdout);
+    while (true) {
+        fd_set fdset;
+        FD_ZERO(&fdset);
+        FD_SET(user_fd, &fdset);
+        FD_SET(udp_fd, &fdset);
+        int nfds = max(user_fd, udp_fd) + 1;
+
+        int n = select(nfds, &fdset, NULL, NULL, NULL);
+        if (n == -1) {
+            perror("select failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (FD_ISSET(udp_fd, &fdset)) {
+            int n = recvfrom(udp_fd, message, 1024, 0, (struct sockaddr*)&peer_addr, &addrlen);
+            if (n < 0) {
+                perror("recv failed");
+                exit(EXIT_FAILURE);
+            }
+
+            char* sender_ip = inet_ntoa(peer_addr.sin_addr);
+            int sender_port = ntohs(peer_addr.sin_port);
+
+            if (sender_ip == NULL) {
+                printf("invalid chat message received\n");
+                exit(EXIT_FAILURE);
+            }
+
+            printf("\rMensagem recebida de %s:%d: %s\n> ", sender_ip, sender_port, message);
+        }
+
+        if (FD_ISSET(0, &fdset)) {
+            printf("> ");
+            fflush(stdout);
+
+            if (fgets(message, 1024, stdin) == NULL) {
+                continue;
+            }
+
+            int n = sendto(udp_fd, message, strlen(message), 0, (struct sockaddr*)&peer_addr, sizeof(peer_addr));
+            if (n < 0) {
+                perror("send failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ src/server.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+
+#define PORT 8080
+#define BACKLOG 10
+
+struct client_info {
+    int fd;
+    struct sockaddr_in addr;
+};
+
+void handle_child_termination(int signal) {
+    pid_t pid;
+    int stat;
+    if ((pid = wait(&stat)) < 0) {
+        perror("wait");
+    }
+}
+
+int main() {
+    int listenfd;
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // set socket options
+    // int opt = 1;
+    // if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    //     perror("setsockopt");
+    //     exit(EXIT_FAILURE);
+    // }
+
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(listenfd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(listenfd, BACKLOG) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    int udp_fd;
+    if ((udp_fd = socket(AF_INET, SOCK_DGRAM, 0)) == 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    if (bind(udp_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    signal(SIGCHLD, handle_child_termination);
+
+    struct client_info clients[100];
+    int num_clients = 0;
+    while (true) {
+        int connfd;
+        struct sockaddr_in client_address;
+        int addrlen = sizeof(address);
+        if ((connfd = accept(listenfd, (struct sockaddr*)&client_address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            continue;
+        }
+
+        in_port_t port;
+        int n = recv(connfd, &port, sizeof(port), 0);
+        if (n < 0) {
+            perror("recv failed");
+            exit(EXIT_FAILURE);
+        }
+
+        client_address.sin_port = port;
+
+        clients[num_clients].fd = connfd;
+        clients[num_clients].addr = client_address;
+        num_clients++;
+
+        printf("Client connected: %d with port %d (of %d)\n", connfd, port, num_clients);
+
+        char client_list_buf[1024] = {0};
+        int client_list_buf_index = 0;
+        for (int i = 0; i < num_clients; i++) {
+            client_list_buf_index += sprintf(client_list_buf + client_list_buf_index, "%d\n", clients[i].fd);
+        }
+
+        for (int i = 0; i < num_clients; i++) {
+            send(clients[i].fd, client_list_buf, strlen(client_list_buf), 0);
+        }
+
+        int pid = fork();
+        if (pid == 0) {
+            close(listenfd);
+            
+            while (true) {
+                int target;
+                
+                int n = recv(connfd, &target, sizeof(target), 0);
+                if (n < 0) {
+                    perror("recv failed");
+                    exit(EXIT_FAILURE);
+                }
+
+                printf("Connection request: %d (from client %d)\n", target, connfd);
+
+                for (int i = 0; i < num_clients; i++) {
+                    if (clients[i].fd == target) {
+                        int n = send(connfd, &clients[i].addr, sizeof(struct sockaddr_in), 0);
+                        if (n < 0) {
+                            perror("send failed");
+                            exit(EXIT_FAILURE);
+                        }
+                        break;
+                    }
+                }
+            }
+        } else if (pid == -1) {
+            perror("fork");
+            continue;
+        }
+    }
+
+    return 0;
+}
